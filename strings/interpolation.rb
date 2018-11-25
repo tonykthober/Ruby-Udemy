3950413921af16cd2715c9484207494ddb25b93d
Anthony Thober <tonykthober@gmail.com>
2018-11-25 13:38:45 -0600

    updating strings branch

---
+++ strings/interpolation.rb
@@ -0,0 +1,7 @@
+#Interpolation is adding #{} to the output to make adding variables easier
+
+text1 = "Hello Ruby"
+text2 = "I am a new programmer"
+text3 = 'How am I doing'
+
+puts "#{text1} #{text2} #{text3}"
