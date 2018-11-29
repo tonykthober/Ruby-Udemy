093d6a881960b2fb2b3308656bc0319639545d0c
Anthony Thober <tonykthober@gmail.com>
2018-11-28 19:22:55 -0600

    pushing loops files

---
+++ Loops/else_if.rb
@@ -0,0 +1,15 @@
+system "cls"
+
+num_1 = 12
+num_2 = 15
+num_3 = 44
+string_1 = "abc"
+string_2 = "xyz"
+
+if num_1 == num_2
+  puts "num 1 is equal to num 2"
+elsif num_1 > num_3
+  puts "num 1 is greater than num 3"
+elsif string_1 != string_2
+  puts "the strings don't match"
+end
---
+++ Loops/unless.rb
@@ -0,0 +1,27 @@
+system "cls"
+
+#how to turn a paragragh into an array
+bacon_array = %w(Ex ham drumstick lorem est kevin. Eiusmod prosciutto consectetur cupidatat, in sirloin dolore cupim elit excepteur magna irure cow short ribs. Anim dolor dolore consectetur tail jerky duis pork belly aute sed aliquip shank elit. Picanha venison ground round, chuck meatloaf flank do shoulder.)
+
+#printing it out
+# p bacon_array
+
+#Prints out only if array is filled
+# unless bacon_array.empty?
+#   puts bacon_array
+# end
+
+#Prints out only if array is empty
+# unless !bacon_array.empty?
+#   puts bacon_array
+# end
+
+#Prints out only if array is empty
+# if bacon_array.empty?
+#   puts bacon_array
+# end
+
+#Prints out only if array is filled
+if !bacon_array.empty?
+  puts bacon_array
+end
